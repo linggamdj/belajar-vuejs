25571887ac98f578e0ea8ef27f16602609ed3b4c
Lingga Adi Atmadja <linggaadiatmdj@gmail.com>
2021-06-02 12:09:24 +0700

    v-if/else/for

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import * as Vue from "vue/dist/vue.esm-bundler.js";
+
+const app = Vue.createApp({
+  template: `
+    <button v-on:click="increment">Increment</button>
+    <p>{{count}}</p>
+    
+    <div v-for="number in numbers">
+      {{number}}
+      <span v-if="isEven(number)">
+        Even
+      </span>
+      <span v-else>
+        Odd
+      </span>
+    </div>
+  `,
+
+  data() {
+    return {
+      count: 0,
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    };
+  },
+
+  methods: {
+    increment() {
+      this.count += 1;
+    },
+
+    isEven(number) {
+      return number % 2 === 0;
+    },
+  },
+});
+
+app.mount("#app");
