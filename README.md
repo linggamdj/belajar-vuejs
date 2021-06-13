2fcdac651fe9b981903cea64337e69b73d88a256
Lingga Adi Atmadja <linggaadiatmdj@gmail.com>
2021-06-13 18:51:58 +0700

    composite example

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # belajar-vuejs
 
-## installation
+## Starter
 
 - yarn add vue@3.0.7
 - yarn add vite@2.1.1 --dev
+- yarn add @vue/compiler-sfc@3.0.7
+- yarn add @vitejs/plugin-vue@1.1.5 --dev
 
-## run
+## Run
 
-- yarn vite src
+- yarn vite --config vite.config.js <project>
---
+++ composition/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="increment">{{ count }}</button>
+</template>
+
+<script>
+// For making obj reactive (example: count variable)
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const count = ref(0);
+    const increment = () => {
+      count.value++;
+    };
+
+    return {
+      count,
+      increment,
+    };
+  },
+};
+</script>
+
+<style scoped>
+button {
+  width: 100px;
+  height: 100px;
+  font-size: 50px;
+}
+</style>
