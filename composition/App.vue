da81bb2fe38fa92feec08b25214f9f87604a1c03
Lingga Adi Atmadja <linggaadiatmdj@gmail.com>
2021-06-14 11:51:03 +0700

    ref and reactive example

--- composition/App.vue
+++ composition/App.vue
@@ -1,20 +1,34 @@
 <template>
   <button @click="increment">{{ count }}</button>
+  <button @click="increase('a')">{{ numbers.a }}</button>
+  <button @click="increase('b')">{{ numbers.b }}</button>
 </template>
 
 <script>
-// For making obj reactive (example: count variable)
-import { ref } from "vue";
+// ref -> still manage to uses primitive types (number, string)
+// reactive -> must pass it first into an object
+import { ref, reactive } from "vue";
 
 export default {
   setup() {
     const count = ref(0);
+    const numbers = reactive({
+      a: 0,
+      b: 0,
+    });
+
+    const increase = (n) => {
+      numbers[n]++;
+    };
+
     const increment = () => {
       count.value++;
     };
 
     return {
       count,
+      numbers,
+      increase,
       increment,
     };
   },
