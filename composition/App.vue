7ae88ad4e5eefdaa42719485dfee7cd6bc0c3513
Lingga Adi Atmadja <linggaadiatmdj@gmail.com>
2021-06-14 12:40:23 +0700

    watch example

--- composition/App.vue
+++ composition/App.vue
@@ -1,42 +1,47 @@
 <template>
   <button @click="increment">{{ count }}</button>
-  <button @click="increase('a')">{{ numbers.a }}</button>
-  <button @click="increase('b')">{{ numbers.b }}</button>
+  <button @click="a++">{{ a }}</button>
+  <button @click="b++">{{ b }}</button>
   <p>{{ total }}</p>
+  <div v-for="number in history" :key="number">
+    {{ number }}
+  </div>
 </template>
 
 <script>
-import { ref, reactive, computed } from "vue";
+import { ref, reactive, computed, watch, watchEffect } from "vue";
 
 export default {
   setup() {
-    // ref -> still manage to uses primitive types (number, string)
     const count = ref(0);
-
-    // reactive -> must pass it first into an object
-    const numbers = reactive({
-      a: 0,
-      b: 0,
-    });
-
-    const increase = (n) => {
-      numbers[n]++;
-    };
+    const a = ref(0);
+    const b = ref(0);
+    const history = ref([]);
 
     const increment = () => {
       count.value++;
     };
 
-    // computed example
     const total = computed(() => {
-      return count.value + numbers.a + numbers.b;
+      return count.value + a.value + b.value;
+    });
+
+    watch([a, b], ([newA, newB], [oldA, oldB]) => {
+      if (newA !== oldA) {
+        history.value.push(`A: ${oldA} -> ${newA}`);
+      }
+
+      if (newB !== oldB) {
+        history.value.push(`B: ${oldB} -> ${newB}`);
+      }
     });
 
     return {
+      a,
+      b,
+      history,
       count,
-      numbers,
       total,
-      increase,
       increment,
     };
   },
